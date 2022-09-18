736647371d8360417c2198ae2ace2a284985444d
david <davidisairias24@gmail.com>
2022-09-18 14:41:20 -0500

    punto 1

--- Tarea 2/tarea_2_p_1.py
+++ Tarea 2/tarea_2_p_1.py
@@ -17,6 +17,7 @@ distance = {}
 
 a = 0
 
+# Calcular distancias
 for i in range(26):
     
     lat_1 = df.iloc[i,0]
@@ -31,27 +32,99 @@ for i in range(26):
         
         d = 2*6378*np.arcsin(root)
         distance[(L[i],L[j])] = d
-        
-m = Model("MVRP")
+
+# Demandas
+d = {}
+for i in range(1,26):
+    d[L[i]] = df.iloc[i,2]
+
+m = Model("MVRP");
 x = m.addVars(L,L,vtype=GRB.BINARY,name='x')
 
 # Cada tienda es visitado
-for j in L[1:-1]:
+for j in L[1:]:
     m.addConstr(quicksum(x[i,j] for i in L)==1)
     
 # Cada visita sale del nodo
-for i in L[1:-1]:
+for i in L[1:]:
     m.addConstr(quicksum(x[i,j] for j in L)==1)
     
 # Del depósito salen K camiones. K=5
-m.addConstr(quicksum(x[L[0],j] for j in L[1:-1])==5)
+m.addConstr(quicksum(x[L[0],j] for j in L[1:])==5)
 
 # Al depósito ingresan k camiones. K=5
-m.addConstr(quicksum(x[i,L[0]] for i in L[1:-1])==5)
+m.addConstr(quicksum(x[i,L[0]] for i in L[1:])==5)
 
 # Evitar ciclos entre ubicaciones
 for i in L:
     for j in L:
         m.addConstr(x[i,j]+x[j,i]<=1)
 
-print(L[1:-1])
+# función objetivo
+m.setObjective(quicksum(distance[i,j]*x[i,j] for i in L for j in L),GRB.MINIMIZE)
+
+m.update()
+
+m.setParam("Outputflag",0)
+m.optimize()
+
+z = m.getObjective().getValue()
+
+rutas = []
+
+for i,j in x.keys():
+    if x[i,j].x>0:
+        rutas.append((i,j))
+
+def calcular_rutas(i:int):
+    r = []
+    nombre = str(i+1)
+    inicio = rutas[i][0]
+    fin = rutas[i][1]
+    r.extend((inicio,fin))  #r.append(inicio)  #r.append(fin)
+    
+    terminar = False
+    j = i+5
+    
+    while (terminar != True) and (j<30):
+        inicio = rutas[j][0]
+        siguiente = rutas[j][1]
+        if inicio==fin and siguiente=="Depósito":
+            terminar=True
+            r.append("Depósito")
+        elif inicio==fin:
+            r.append(siguiente)
+            fin = siguiente
+            j = i+5
+        else:
+            j = j+1
+    return r
+
+def calcular_distancias(r:list):
+    distancias = []
+    for ruta in r:
+        dist = 0
+        for i in range(len(ruta)-1):
+            dist = dist + distance[ruta[i],ruta[i+1]]
+        distancias.append(dist)
+    return distancias
+
+def calcular_demanda(r:list):
+    demanda = []
+    for ruta in r:
+        dist = 0
+        for i in range(1,len(ruta)-1):
+            dist = dist + d[ruta[i]]
+        demanda.append(dist)
+    return demanda
+
+num = [i+1 for i in range(5)]
+rutas = [calcular_rutas(i) for i in range(5)]
+energia = [ i*1.5 for i in calcular_distancias(rutas)]
+demanda = calcular_demanda(rutas)
+
+dic = {"Ruta":num,"Secuencia":rutas,"Energía":energia,"Demanda":demanda}
+dic = pd.DataFrame.from_dict(dic)
+print(dic)
+print(z)
+print(np.sum(calcular_distancias(rutas)))
